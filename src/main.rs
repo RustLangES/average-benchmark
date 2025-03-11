b32e6cc379435bcf2ab567961d915bbcb3661d66
Jose <quijadajosed@gmail.com>
2025-03-11 02:23:21 -0300

    refactor benchmark scoring to use f64 for improved precision and update output formatting

--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,31 @@ fn compute_range(start: u128, end: u128) -> u128 {
     sum
 }
 
-fn compute_score(benchmark_time: std::time::Duration) -> u128 {
+fn compute_score(benchmark_time: std::time::Duration) -> f64 {
     let time_in_seconds = benchmark_time.as_secs_f64();
     if time_in_seconds > 0.0 {
-        (ITERATIONS as f64 / time_in_seconds) as u128
+        (ITERATIONS as f64 / time_in_seconds) / 1_000_00.0
     } else {
-        0
+        0.0
     }
 }
 
-fn benchmark_single_thread() -> u128 {
+fn benchmark_single_thread() -> f64 {
     let start = Instant::now();
     let result = compute_range(0, ITERATIONS);
     let duration = start.elapsed();
-    let score = compute_score(duration);
+    let score: f64 = compute_score(duration);
 
     println!(
         "\x1B[33mSingle-thread time\x1B[0m: \x1B[36m{:.3}s\x1B[0m",
         duration.as_secs_f64()
     );
-    println!("\x1B[33mSingle-thread result\x1B[0m: {}", result);
-    println!("\x1B[32mSingle-thread score\x1B[0m: {}", score);
+    println!("\x1B[33mSingle-thread result\x1B[0m: {:.3e}", result);
+    println!("\x1B[32mSingle-thread score\x1B[0m: {}", format!("{:.3}", score));
     return score;
 }
 
-fn benchmark_multi_thread(num_threads: usize) -> u128 {
+fn benchmark_multi_thread(num_threads: usize) -> f64 {
     let chunk_size: u128 = ITERATIONS / num_threads as u128;
     let mut handles: Vec<thread::JoinHandle<(u128, std::time::Duration, usize)>> = Vec::new();
     let start: Instant = Instant::now();
@@ -87,14 +87,14 @@ fn benchmark_multi_thread(num_threads: usize) -> u128 {
     }
 
     let duration = start.elapsed();
-    let score = compute_score(duration);
+    let score: f64 = compute_score(duration);
 
     println!(
         "\x1B[33mMulti-thread total time\x1B[0m: \x1B[36m{:.2?}\x1B[0m",
         duration
     );
-    println!("\x1B[33mMulti-thread result\x1B[0m: {}", total_sum);
-    println!("\x1B[32mMulti-thread score\x1B[0m: {}", score);
+    println!("\x1B[33mMulti-thread result\x1B[0m: {:.3e}", total_sum);
+    println!("\x1B[32mMulti-thread score\x1B[0m: {}", format!("{:.3}", score));
 
     println!(
         "\x1B[33mMin thread time\x1B[0m: {:.2?} (\x1B[32mThread {}\x1B[0m)",
@@ -133,7 +133,7 @@ fn ask_to_send() -> bool {
     }
 }
 
-async fn send_data(score_single_thread: u128, score_multi_thread: u128) -> Result<(), Box<dyn std::error::Error>> {
+async fn send_data(score_single_thread: f64, score_multi_thread: f64) -> Result<(), Box<dyn std::error::Error>> {
     use sysinfo::System;
     let mut sys = System::new_all();
     sys.refresh_all();
@@ -149,8 +149,8 @@ async fn send_data(score_single_thread: u128, score_multi_thread: u128) -> Resul
         "cpu_vendor_id": sys.cpus().first().map(|cpu| cpu.vendor_id()),
         "cpu_brand": sys.cpus().first().map(|cpu| cpu.brand()),
         "cpu_frequency": sys.cpus().first().map(|cpu| cpu.frequency()),
-        "score_single_thread": score_single_thread,
-        "score_multi_thread": score_multi_thread,
+        "score_single_thread": format!("{:.3}", score_single_thread),
+        "score_multi_thread": format!("{:.3}", score_multi_thread),
     });
 
     let client = reqwest::Client::builder().build()?;
@@ -191,27 +191,30 @@ async fn send_data(score_single_thread: u128, score_multi_thread: u128) -> Resul
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let baner: &str = r#"
-    _____ ______ _   _  ______                 _                          _     
-    /  __ \| ___ \ | | | | ___ \               | |                        | |    
-    | /  \/| |_/ / | | | | |_/ / ___ _ __   ___| |__  _ __ ___   __ _ _ __| | __ 
-    | |    |  __/| | | | | ___ \/ _ \ '_ \ / __| '_ \| '_ ` _ \ / _` | '__| |/ / 
-    | \__/\| |   | |_| | | |_/ /  __/ | | | (__| | | | | | | | | (_| | |  |   <  
-     \____/\_|    \___/  \____/ \___|_| |_|\___|_| |_|_| |_| |_|\__,_|_|  |_|\_\                                                                                                       
-                           _        ______          _                            
-                          (_)       | ___ \        | |                           
-                           _ _ __   | |_/ /   _ ___| |_                          
-                          | | '_ \  |    / | | / __| __|                         
-                          | | | | | | |\ \ |_| \__ \ |_                          
-                          |_|_| |_| \_| \_\__,_|___/\__|"#;
+     _____ ______ _   _  ______                 _                          _         #++++**
+    /  __ \| ___ \ | | | | ___ \               | |                        | |       -*----#
+    | /  \/| |_/ / | | | | |_/ / ___ _ __   ___| |__  _ __ ___   __ _ _ __| | __    *----#
+    | |    |  __/| | | | | ___ \/ _ \ '_ \ / __| '_ \| '_ ` _ \ / _` | '__| |/ /   -*---*+===
+    | \__/\| |   | |_| | | |_/ /  __/ | | | (__| | | | | | | | | (_| | |  |   <    *-------*=
+     \____/\_|    \___/  \____/ \___|_| |_|\___|_| |_|_| |_| |_|\__,_|_|  |_|\_\   *++++--*:
+                                                                                      *==*
+                                                                                     -*++
+                                                                                     ##:
+                                                                                    =#
+                                          
+                          "#;
     println!("\x1B[34m{}\x1B[0m", baner);
-    let score_single_thread: u128 = benchmark_single_thread();
+
+//     "score_single_thread": format!("{:.3}", score_single_thread),
+// "score_multi_thread": format!("{:.3}", score_multi_thread),
+    let score_single_thread: f64 = benchmark_single_thread();
 
     let num_threads: usize = num_cpus::get();
     println!(
         "\x1B[33mLogical cores numbers detected\x1B[0m: \x1B[36m{}\x1B[0m",
         num_threads
     );
-    let score_multi_thread: u128 = benchmark_multi_thread(num_threads);
+    let score_multi_thread: f64 = benchmark_multi_thread(num_threads);
     if ask_to_send() {
         send_data(score_multi_thread, score_single_thread).await?;
     } else {
