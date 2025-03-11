2243eacbcdfc7b82ac18900b9f5d30296c324618
Jose <quijadajosed@gmail.com>
2025-03-11 01:09:05 -0300

    add async data sending feature and enhance benchmarking scores

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::hint::black_box;
-use std::time::Instant;
-use std::thread;
+use std::io;
 use num_cpus;
+use std::hint::black_box;
+use std::thread;
+use std::time::Instant;
+use reqwest::header::HeaderMap;
+use serde_json::Value;
+use serde_json::json;
+use chrono::{DateTime};
 
-const ITERATIONS: u128 = 20_000_000_000;
+const ITERATIONS: u128 = 1_000_000_000;
 
 fn compute_range(start: u128, end: u128) -> u128 {
     let mut sum: u128 = 0;
@@ -14,32 +19,48 @@ fn compute_range(start: u128, end: u128) -> u128 {
     sum
 }
 
-fn benchmark_single_thread() {
-    let start: Instant = Instant::now();
-    let result: u128 = compute_range(0, ITERATIONS);
-    let duration: std::time::Duration = start.elapsed();
-    println!("\x1B[33mSingle-thread time\x1B[0m: \x1B[36m{:.3}s\x1B[0m", duration.as_secs_f64());
-    println!("\x1B[33mSingle-thread result\x1B[0m: {}", result);
+fn compute_score(benchmark_time: std::time::Duration) -> u128 {
+    let time_in_seconds = benchmark_time.as_secs_f64();
+    if time_in_seconds > 0.0 {
+        (ITERATIONS as f64 / time_in_seconds) as u128
+    } else {
+        0
+    }
 }
 
-fn benchmark_multi_thread(num_threads: usize) {
+fn benchmark_single_thread() -> u128 {
+    let start = Instant::now();
+    let result = compute_range(0, ITERATIONS);
+    let duration = start.elapsed();
+    let score = compute_score(duration);
+
+    println!(
+        "\x1B[33mSingle-thread time\x1B[0m: \x1B[36m{:.3}s\x1B[0m",
+        duration.as_secs_f64()
+    );
+    println!("\x1B[33mSingle-thread result\x1B[0m: {}", result);
+    println!("\x1B[32mSingle-thread score\x1B[0m: {}", score);
+    return score;
+}
+
+fn benchmark_multi_thread(num_threads: usize) -> u128 {
     let chunk_size: u128 = ITERATIONS / num_threads as u128;
     let mut handles: Vec<thread::JoinHandle<(u128, std::time::Duration, usize)>> = Vec::new();
     let start: Instant = Instant::now();
 
     for i in 0..num_threads {
-        let range_start: u128 = i as u128 * chunk_size;
-        let range_end: u128 = if i == num_threads - 1 {
+        let range_start = i as u128 * chunk_size;
+        let range_end = if i == num_threads - 1 {
             ITERATIONS
         } else {
             (i as u128 + 1) * chunk_size
         };
 
-        let handle: thread::JoinHandle<(u128, std::time::Duration, usize)> = thread::spawn(move || {
-            let thread_start: Instant = Instant::now();
-            let result: u128 = compute_range(range_start, range_end);
-            let duration: std::time::Duration = thread_start.elapsed();
-            (result, duration, i) // Retorna el resultado, el tiempo y el ID del hilo
+        let handle = thread::spawn(move || {
+            let thread_start = Instant::now();
+            let result = compute_range(range_start, range_end);
+            let duration = thread_start.elapsed();
+            (result, duration, i)
         });
 
         handles.push(handle);
@@ -65,9 +86,15 @@ fn benchmark_multi_thread(num_threads: usize) {
         }
     }
 
-    let duration: std::time::Duration = start.elapsed();
-    println!("\x1B[33mMulti-thread total time\x1B[0m: \x1B[36m{:.2?}\x1B[0m", duration);
+    let duration = start.elapsed();
+    let score = compute_score(duration);
+
+    println!(
+        "\x1B[33mMulti-thread total time\x1B[0m: \x1B[36m{:.2?}\x1B[0m",
+        duration
+    );
     println!("\x1B[33mMulti-thread result\x1B[0m: {}", total_sum);
+    println!("\x1B[32mMulti-thread score\x1B[0m: {}", score);
 
     println!(
         "\x1B[33mMin thread time\x1B[0m: {:.2?} (\x1B[32mThread {}\x1B[0m)",
@@ -77,28 +104,124 @@ fn benchmark_multi_thread(num_threads: usize) {
         "\x1B[33mMax thread time\x1B[0m: {:.2?} (\x1B[32mThread {}\x1B[0m)",
         max_time, max_thread
     );
+    return score;
 }
 
+fn ask_to_send() -> bool {
+    println!("¿Desea enviar los datos de la prueba al servidor? (y/n)");
 
-fn main() {
+    let mut respuesta: String = String::new();
+
+    loop {
+        match io::stdin().read_line(&mut respuesta) {
+            Ok(_) => {
+                let input = respuesta.trim().to_lowercase();
+                if input == "y" {
+                    return true;
+                } else if input == "n" {
+                    return false;
+                } else {
+                    println!("Entrada no válida. Por favor, presione 'y' para sí o 'n' para no.");
+                    respuesta.clear();
+                }
+            }
+            Err(_) => {
+                println!("Error al leer la entrada. Intente nuevamente.");
+                respuesta.clear();
+            }
+        }
+    }
+}
+
+async fn send_data(score_single_thread: u128, score_multi_thread: u128) -> Result<(), Box<dyn std::error::Error>> {
+    use sysinfo::System;
+    // CPUs and processes are filled!
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    
+    let system_name = System::name().unwrap_or_else(|| "Unknown".to_string());
+    let system_os_version = System::os_version().unwrap_or_else(|| "Unknown".to_string());
+    let combined_system_info = format!("{} {}", system_name, system_os_version);
+
+    let system_info = json!({
+        "system_info": combined_system_info,
+        "system_host_name": System::host_name(),
+        "number_of_cpus": sys.cpus().len(),
+        "cpu_vendor_id": sys.cpus().first().map(|cpu| cpu.vendor_id()),
+        "cpu_brand": sys.cpus().first().map(|cpu| cpu.brand()),
+        "cpu_frequency": sys.cpus().first().map(|cpu| cpu.frequency()),
+        "score_single_thread": score_single_thread,
+        "score_multi_thread": score_multi_thread,
+    });
+
+    let client = reqwest::Client::builder().build()?;
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse()?);
+
+    let request = client
+    .request(reqwest::Method::POST, "http://localhost:8080/submit-tests")
+    .headers(headers)
+        .json(&system_info);
+
+    let response = request.send().await?;
+    let body = response.text().await?;
+    let parsed: Value = serde_json::from_str(&body)?;
+    
+    
+
+    let success = parsed["success"].as_bool() == Some(true);
+    let message_type = if success { "Message" } else { "Error" };
+    let color_code = if success { "32" } else { "31" };
+    let content = if success {
+        parsed["message"].as_str().unwrap_or("Success")
+    } else {
+        parsed["error"].as_str().unwrap_or("Unknown error")
+    };
+
+    let timestamp = parsed["timestamp"]
+        .as_str()
+        .and_then(|ts_str| DateTime::parse_from_rfc3339(ts_str).ok())
+        .map(|ts| ts.format("%Y-%m-%d %H:%M:%S UTC").to_string())
+        .unwrap_or_else(|| "N/A".to_string());
+
+    println!("\x1B[{}m{}: {}\x1B[0m", color_code, message_type, content);
+    println!("\x1B[{}mTimestamp: {}\x1B[0m", color_code, timestamp);
+
+    
+    // ...existing code...
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let baner: &str = r#"
     _____ ______ _   _  ______                 _                          _     
     /  __ \| ___ \ | | | | ___ \               | |                        | |    
     | /  \/| |_/ / | | | | |_/ / ___ _ __   ___| |__  _ __ ___   __ _ _ __| | __ 
     | |    |  __/| | | | | ___ \/ _ \ '_ \ / __| '_ \| '_ ` _ \ / _` | '__| |/ / 
     | \__/\| |   | |_| | | |_/ /  __/ | | | (__| | | | | | | | | (_| | |  |   <  
-     \____/\_|    \___/  \____/ \___|_| |_|\___|_| |_|_| |_| |_|\__,_|_|  |_|\_\ 
-                                                                                                                                               
+     \____/\_|    \___/  \____/ \___|_| |_|\___|_| |_|_| |_| |_|\__,_|_|  |_|\_\                                                                                                       
                            _        ______          _                            
                           (_)       | ___ \        | |                           
                            _ _ __   | |_/ /   _ ___| |_                          
                           | | '_ \  |    / | | / __| __|                         
                           | | | | | | |\ \ |_| \__ \ |_                          
-                          |_|_| |_| \_| \_\__,_|___/\__|"#;                                                    
+                          |_|_| |_| \_| \_\__,_|___/\__|"#;
     println!("\x1B[34m{}\x1B[0m", baner);
-    benchmark_single_thread();
+    let score_single_thread: u128 = benchmark_single_thread();
 
     let num_threads: usize = num_cpus::get();
-    println!("\x1B[33mLogical cores numbers detected\x1B[0m: \x1B[36m{}\x1B[0m", num_threads);
-    benchmark_multi_thread(num_threads);
+    println!(
+        "\x1B[33mLogical cores numbers detected\x1B[0m: \x1B[36m{}\x1B[0m",
+        num_threads
+    );
+    let score_multi_thread: u128 = benchmark_multi_thread(num_threads);
+    if ask_to_send() {
+        send_data(score_multi_thread, score_single_thread).await?;
+    } else {
+        println!("Datos no enviados.");
+    }
+
+    Ok(())
 }
