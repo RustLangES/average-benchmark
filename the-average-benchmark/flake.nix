f21b1fe2222efb7aaffe1dd9e3da68a768978827
Juanperias <136520331+Juanperias@users.noreply.github.com>
2025-03-16 10:45:19 -0400

    feat: add nix flake

--- the-average-benchmark/flake.nix
+++ the-average-benchmark/flake.nix
@@ -6,24 +6,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.rustup                   # Rustup para gestionar Rust
-            pkgs.lldb                     # Debugger LLDB
-            pkgs.rust-analyzer             # Soporte de Rust en VS Code
-            pkgs.vscode-extensions.vadimcn.vscode-lldb  # Extensión CodeLLDB
-            pkgs.libllvm                   # Dependencia de LLDB en NixOS
-          ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rustup # Rustup para gestionar Rust
+          pkgs.lldb # Debugger LLDB
+          pkgs.rust-analyzer # Soporte de Rust en VS Code
+          pkgs.vscode-extensions.vadimcn.vscode-lldb # Extensión CodeLLDB
+          pkgs.libllvm # Dependencia de LLDB en NixOS
+        ];
 
-          shellHook = ''
-            rustup show active-toolchain > /dev/null 2>&1 || rustup default stable
-          '';
-        };
-      });
+        shellHook = ''
+          rustup show active-toolchain > /dev/null 2>&1 || rustup default stable
+        '';
+      };
+    });
 }
