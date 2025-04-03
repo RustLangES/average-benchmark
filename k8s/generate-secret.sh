9c8e113877f89c99d295ed1fb9383c30800ba5cc
José Quijada <quijadajosed@gmail.com>
2025-04-02 21:08:54 -0300

    feat: update Kubernetes manifests for benchmark API and improve secret generation script

--- k8s/generate-secret.sh
+++ k8s/generate-secret.sh
@@ -3,26 +3,27 @@
 # Obtener la ruta del script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Definir la ruta del archivo .env
 ENV_FILE="$SCRIPT_DIR/../.env"
 
-# Verificar si el archivo .env existe
 if [ ! -f "$ENV_FILE" ]; then
     echo "❌ Error: Archivo .env no encontrado en $ENV_FILE"
     exit 1
 fi
 
-# Cargar las variables del .env
-export $(grep -v '^#' "$ENV_FILE" | xargs)
+# Cargar las variables del .env de forma segura
+set -o allexport
+source "$ENV_FILE"
+set +o allexport
 
-# Verificar si la variable está definida
 if [ -z "$DISCORD_WEBHOOK_URL" ]; then
     echo "❌ Error: DISCORD_WEBHOOK_URL no está definida en el archivo .env"
     exit 1
 fi
 
-# Definir la salida del secret.yaml
 SECRET_FILE="$SCRIPT_DIR/secret.yaml"
+
+# Eliminar el archivo si ya existe
+rm -f "$SECRET_FILE"
 
 # Generar el archivo de secreto
 cat << EOF > "$SECRET_FILE"
@@ -39,7 +40,7 @@ EOF
 echo "✅ Secreto generado correctamente en $SECRET_FILE"
 
 # Aplicar los manifiestos de Kubernetes
-k8s kubectl apply -f "$SCRIPT_DIR/namespace.yaml"
-k8s kubectl apply -f "$SECRET_FILE"
-k8s kubectl apply -f "$SCRIPT_DIR/deployment.yaml"
-k8s kubectl apply -f "$SCRIPT_DIR/service.yaml"
+kubectl apply -f "$SCRIPT_DIR/namespace.yaml"
+kubectl apply -f "$SECRET_FILE"
+kubectl apply -f "$SCRIPT_DIR/deployment.yaml"
+kubectl apply -f "$SCRIPT_DIR/service.yaml"
