f6df7d98096801157cea2820ea26ad8101d5163c
victoria.casasampere@beethedata.com <victoria.casasampere@beethedata.com>
2025-06-13 15:05:09 +0200

    Optimise benchmark for accuracy and consistency

--- average-benchmark/src/benchmark.rs
+++ average-benchmark/src/benchmark.rs
@@ -1,16 +1,15 @@
 use std::hint::black_box;
-use std::thread;
 use std::time::Instant;
 
-pub const ITERATIONS: u128 = 1_000_000_000;
+use rayon::prelude::*;
 
-pub fn compute_range(start: u128, end: u128) -> u128 {
-    let mut sum: u128 = 0;
-    for i in start..end {
-        // Performs a * b + c operation (i * i + i) with wrapping and black_box to accurately benchmark cpu performance.
-        sum = sum.wrapping_add(black_box(i.wrapping_mul(i).wrapping_add(i)));
-    }
-    sum
+pub const ITERATIONS: usize = 10_000_000_000;
+
+#[inline]
+/// Performs a * b + c operation (i * i + i)
+/// with wrapping and black_box to accurately benchmark cpu performance.
+pub fn compute(sum: &mut usize, value: usize) {
+    *sum = sum.wrapping_add(black_box(value.wrapping_mul(value).wrapping_add(value)));
 }
 
 pub fn compute_score(benchmark_time: std::time::Duration) -> f64 {
@@ -24,8 +23,14 @@ pub fn compute_score(benchmark_time: std::time::Duration) -> f64 {
 
 pub fn benchmark_single_thread() -> f64 {
     let start = Instant::now();
-    let result = compute_range(0, ITERATIONS);
+
+    let mut result = 0;
+    for i in 0..ITERATIONS {
+        compute(&mut result, i);
+    }
+
     let duration = start.elapsed();
+
     let score: f64 = compute_score(duration);
 
     println!(
@@ -41,49 +46,59 @@ pub fn benchmark_single_thread() -> f64 {
 }
 
 pub fn benchmark_multi_thread(num_threads: usize) -> f64 {
-    let chunk_size: u128 = ITERATIONS / num_threads as u128;
-    let mut handles: Vec<thread::JoinHandle<(u128, std::time::Duration, usize)>> = Vec::new();
-    let start: Instant = Instant::now();
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(num_threads)
+        .build()
+        .unwrap();
 
-    for i in 0..num_threads {
-        let range_start = i as u128 * chunk_size;
-        let range_end = if i == num_threads - 1 {
-            ITERATIONS
-        } else {
-            (i as u128 + 1) * chunk_size
-        };
-
-        let handle = thread::spawn(move || {
-            let thread_start = Instant::now();
-            let result = compute_range(range_start, range_end);
-            let duration = thread_start.elapsed();
-            (result, duration, i)
-        });
-
-        handles.push(handle);
-    }
-
-    let mut total_sum: u128 = 0;
+    let mut total_sum = 0usize;
     let mut min_time = std::time::Duration::MAX;
     let mut max_time = std::time::Duration::ZERO;
     let mut min_thread = 0;
     let mut max_thread = 0;
+    let mut thread_id = 0;
 
-    for handle in handles {
-        let (result, duration, thread_id) = handle.join().unwrap();
-        total_sum = total_sum.wrapping_add(result);
+    let duration = pool.install(|| {
+        let (tx, rx) = std::sync::mpsc::channel();
 
-        if duration < min_time {
-            min_time = duration;
-            min_thread = thread_id;
+        let start: Instant = Instant::now();
+
+        (0..ITERATIONS)
+            .into_par_iter()
+            .fold(
+                || (0usize, Instant::now()),
+                |mut x, i| {
+                    compute(&mut x.0, i);
+                    x
+                },
+            )
+            .for_each(|x| {
+                let _ = tx.send((x.0, x.1));
+            });
+
+        let duration = start.elapsed();
+
+        while let Ok((i, instant)) = rx.try_recv() {
+            total_sum = total_sum.wrapping_add(i);
+
+            let elapsed = instant.elapsed();
+
+            if elapsed < min_time {
+                min_time = duration;
+                min_thread = thread_id;
+            }
+
+            if elapsed > max_time {
+                max_time = duration;
+                max_thread = thread_id;
+            }
+
+            thread_id += 1;
         }
-        if duration > max_time {
-            max_time = duration;
-            max_thread = thread_id;
-        }
-    }
 
-    let duration = start.elapsed();
+        duration
+    });
+
     let score: f64 = compute_score(duration);
 
     println!(
@@ -97,6 +112,10 @@ pub fn benchmark_multi_thread(num_threads: usize) -> f64 {
     );
 
     println!(
+        "\x1B[33mAverage iterations per thread\x1B[0m: {:.2?}",
+        ITERATIONS / thread_id
+    );
+    println!(
         "\x1B[33mMin thread time\x1B[0m: {:.2?} (\x1B[32mThread {}\x1B[0m)",
         min_time, min_thread
     );
@@ -104,5 +123,6 @@ pub fn benchmark_multi_thread(num_threads: usize) -> f64 {
         "\x1B[33mMax thread time\x1B[0m: {:.2?} (\x1B[32mThread {}\x1B[0m)",
         max_time, max_thread
     );
-    return score;
+
+    score
 }
